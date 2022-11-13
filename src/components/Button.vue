07a59f3618d764828318f11e12348c38bedc68a0
Pablo Cuevas <cuevasgpablo@gmail.com>
2022-11-13 16:33:35 -0300

    feat: button ahora recibe clases css

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,20 +1,18 @@
 <script setup lang="ts">
-import { useTransitions } from '@/utils'
+import { useTransitions } from '@/utils';
 
-const { defaultTransition } = useTransitions()
+const { defaultTransition } = useTransitions();
 
 interface ButtonProps {
-  label: string
-  disabled?: boolean
+  label: string;
+  disabled?: boolean;
 }
 
-defineProps<ButtonProps>()
-
+defineProps<ButtonProps>();
 </script>
 <template>
   <button
-    class="py-2 px-4 rounded-full bg-emerald-700 text-emerald-50 focus:outline-none enabled:hover:bg-emerald-900 dark:enabled:hover:bg-emerald-400 dark:enabled:hover:text-neutral-900 dark:bg-emerald-600 disabled:opacity-50 disabled:cursor-default"
-    :class="[defaultTransition]"
+    :class="[defaultTransition, 'py-2 px-4 rounded-full focus:outline-none', $attrs.class]"
     :disabled="disabled"
   >
     {{ label }}
