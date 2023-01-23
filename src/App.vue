877df194310e6911fc25a6a3ffe0bef56fb26b7a
Pablo Cuevas <pcuevas@pegasus.cl>
2023-01-23 14:07:28 -0300

    add: descripcion configuracion actual

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
-import ToggleDark from '@/components/ToggleDark.vue';
-import { useTransitions } from './utils';
-import Copyright from './components/Copyright.vue';
+import { RouterView } from 'vue-router'
+import ToggleDark from '@/components/ToggleDark.vue'
+import { useTransitions } from './utils'
+import Copyright from './components/Copyright.vue'
+import { useOptionsStore } from './stores/options'
+import { computed } from 'vue'
 
-const { defaultTransition } = useTransitions();
+const { defaultTransition } = useTransitions()
+
+const optionsStore = useOptionsStore()
+
+const minimumRut = computed(() => optionsStore.minRut.toLocaleString())
+const maximumRut = computed(() => optionsStore.maxRut.toLocaleString())
 </script>
 
 <template>
@@ -14,6 +21,13 @@ const { defaultTransition } = useTransitions();
   >
     <div class="absolute right-1 top-1">
       <ToggleDark />
+    </div>
+    <div class="absolute left-1 top-1 text-xs text-gray-500">
+      <h1>Configuración Actual:</h1>
+      <ul>
+        <li>Valor RUT Mínimo: {{ minimumRut }}</li>
+        <li>Valor RUT Máximo: {{ maximumRut }}</li>
+      </ul>
     </div>
     <main class="px-5 mt-16 w-full">
       <RouterView />
