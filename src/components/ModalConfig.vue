c2fc7ccd6c04411b7f148a26af08d665d26a0395
Pablo Cuevas <pcuevas@pegasus.cl>
2023-01-23 13:21:53 -0300

    refactor: agregadas opciones para rut minimo y rut maximo

--- src/components/ModalConfig.vue
+++ src/components/ModalConfig.vue
@@ -1,94 +1,76 @@
 <script setup lang="ts">
-import { TransitionRoot, TransitionChild, Dialog, DialogPanel, DialogTitle } from '@headlessui/vue';
-import { ref } from 'vue';
-import { useMagicKeys, whenever, useToggle } from '@vueuse/core';
-import Button from './Button.vue';
+/* eslint-disable no-unused-vars */
+import { ref, reactive } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useMagicKeys, whenever, useToggle } from '@vueuse/core'
+import { DialogTitle } from '@headlessui/vue'
+import { useOptionsStore } from '@/stores/options'
+import Button from './Button.vue'
+import ModalWrapper from './ModalWrapper.vue'
+import InputNumber from './InputNumber.vue'
 
-const keys = useMagicKeys();
+const optionsStore = useOptionsStore()
+const { minRut, maxRut, defaultMinRut } = storeToRefs(optionsStore)
 
-const ctrlK = keys['Ctrl+Shift+K'];
-const escape = keys['escape'];
+const keys = useMagicKeys()
 
-const isOpen = ref(false);
-const toggleOpen = useToggle(isOpen);
+const isOpen = ref(false)
+const toggleOpen = useToggle(isOpen)
 
-whenever(ctrlK, () => toggleOpen());
+whenever(keys['Ctrl+Shift+K'], () => toggleOpen())
 
-whenever(escape, () => closeModal());
+whenever(keys['escape'], () => closeModal())
+
+const rutOptions = reactive({
+  minimum: minRut.value,
+  maximum: maxRut.value
+})
 
 function closeModal() {
-  isOpen.value = false;
+  isOpen.value = false
 }
 
 function guardarConfig() {
-  console.log('guardar config');
-  closeModal();
+  if (rutOptions.minimum < defaultMinRut.value) return
+  if (rutOptions.maximum <= rutOptions.minimum) return
+
+  minRut.value = rutOptions.minimum
+  maxRut.value = rutOptions.maximum
+  closeModal()
+  location.reload()
 }
 </script>
 
 <template>
-  <TransitionRoot
-    appear
-    :show="isOpen"
-    as="template"
-  >
-    <Dialog
-      as="div"
-      @close="closeModal"
-      class="relative z-10"
+  <ModalWrapper :show="isOpen" @close="closeModal">
+    <DialogTitle
+      as="h3"
+      class="text-lg font-medium leading-6 text-zinc-900 dark:text-zinc-50"
     >
-      <TransitionChild
-        as="template"
-        enter="duration-300 ease-out"
-        enter-from="opacity-0"
-        enter-to="opacity-100"
-        leave="duration-200 ease-in"
-        leave-from="opacity-100"
-        leave-to="opacity-0"
-      >
-        <div class="fixed inset-0 bg-black bg-opacity-25" />
-      </TransitionChild>
+      Configuración
+    </DialogTitle>
+    <section class="mt-5 text-zinc-900 dark:text-zinc-50 grid grid-cols-2 gap-2">
+      <InputNumber
+        id="minimum-rut"
+        v-model="rutOptions.minimum"
+        label="Valor mínimo RUT"
+        :min="defaultMinRut"
+      />
+      <InputNumber
+        id="maximum-rut"
+        v-model="rutOptions.maximum"
+        label="Valor máximo RUT"
+        :min="rutOptions.minimum"
+      />
+    </section>
 
-      <div class="fixed inset-0 overflow-y-auto">
-        <div class="flex min-h-full items-center justify-center p-4 text-center">
-          <TransitionChild
-            as="template"
-            enter="duration-300 ease-out"
-            enter-from="opacity-0 scale-95"
-            enter-to="opacity-100 scale-100"
-            leave="duration-200 ease-in"
-            leave-from="opacity-100 scale-100"
-            leave-to="opacity-0 scale-95"
-          >
-            <DialogPanel
-              class="w-full max-w-3xl transform overflow-hidden rounded-2xl p-6 text-left align-middle shadow-xl transition-all bg-zinc-100 dark:bg-zinc-900"
-            >
-              <DialogTitle
-                as="h3"
-                class="text-lg font-medium leading-6 text-zinc-900 dark:text-zinc-50"
-              >
-                Configuración
-              </DialogTitle>
-              <div class="mt-2 text-zinc-900 dark:text-zinc-50">
-                <p class="text-sm">¡En construcción!, estamos trabajando para usted</p>
-              </div>
-
-              <div class="mt-4 flex gap-2 flex-row-reverse">
-                <Button
-                  label="Guardar"
-                  class="btn-primary"
-                  @click="guardarConfig"
-                />
-                <Button
-                  label="Cancelar"
-                  class="hover:bg-emerald-800/70 hover:text-emerald-50"
-                  @click="closeModal"
-                />
-              </div>
-            </DialogPanel>
-          </TransitionChild>
-        </div>
-      </div>
-    </Dialog>
-  </TransitionRoot>
+    <section class="mt-5 flex gap-2 flex-row-reverse">
+      <Button label="Guardar" class="btn-primary" @click="guardarConfig" />
+      <Button
+        label="Cancelar"
+        class="hover:bg-emerald-800/70 hover:text-emerald-50"
+        @click="closeModal"
+      />
+    </section>
+  </ModalWrapper>
 </template>
